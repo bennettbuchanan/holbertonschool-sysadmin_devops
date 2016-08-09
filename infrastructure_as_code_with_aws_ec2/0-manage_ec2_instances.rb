6e3a8b48fc91ab69b160f5bbbeb075a7d2295a18
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-09 00:49:35 -0700

    Add additional functionality and options for handling instances.

--- infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
@@ -4,12 +4,20 @@ require 'optparse'
 require 'aws-sdk'
 require 'yaml'
 
-
 options = {}
-
-OptionParser.new do |parser|
-  parser.on("-aACTION", "--action=ACTION", "Name to say hello to") do |a|
+OptionParser.new do |opts|
+  opts.on("-aACTION", "--action=ACTION", "Action to take.") do |a|
     options[:action] = a
+  end
+  opts.on("-iINSTANCE_ID", "--instance_id=INSTANCE_ID", "ID of the instance.") do |i|
+	  options[:instance_id] = i
+  end
+  opts.on("-v", "--[no-]verbose", "Run verbosely.") do |v|
+    options.verbose = v
+  end
+  opts.on_tail("-h", "--help", "Show help message.") do
+    puts opts
+    exit
   end
 end.parse!
 
@@ -18,12 +26,12 @@ when "launch"
   config = YAML.load_file('config.yaml')
 
   ec2 = Aws::EC2::Client.new({
-        region: 'us-west-2',
-        access_key_id: config['access_key_id'],
-        secret_access_key: config['secret_access_key']
-      })
+    region: 'us-west-2',
+    access_key_id: config['access_key_id'],
+    secret_access_key: config['secret_access_key']
+  })
 
-  this_instance = ec2.run_instances({
+  resp = ec2.run_instances({
     dry_run: false,
     image_id: config['image_id'],
     min_count: 1,
@@ -34,15 +42,31 @@ when "launch"
     placement: {
       availability_zone: config['availability-zone'],
     }
-    })
-when "start"
-  puts "Start."
+  })
+
+  puts resp.instances[0].instance_id
+  resp = ec2.wait_until(:instance_running, instance_ids:[resp.instances[0].instance_id])
+  puts resp.reservations[0].instances[0].public_dns_name
+
 when "stop"
   resp = client.stop_instances({
-  dry_run: false,
-  instance_ids: ["String"],
-  force: false,
+    dry_run: false,
+    instance_ids: [resp.instances[0].instance_id], # required
+    force: false,
   })
+
+when "start"
+  resp = client.start_instances({
+    instance_ids: [resp.instances[0].instance_id], # required
+    dry_run: false,
+  })
+
+  resp = ec2.wait_until(:instance_running, instance_ids:[options[:instance_id]])
+  puts resp.reservations[0].instances[0].public_dns_name
+
 when "terminate"
-  puts "terminate."
+  resp = client.terminate_instances({
+    dry_run: false,
+    instance_ids: [options[:instance_id]], # required
+  })
 end
